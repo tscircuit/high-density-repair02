80e5565a397d4f57fdd2fb1bc5eb072744e9e888
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-03-24 08:44:36 +0200

    fix import

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       lib: resolve(__dirname, "./lib"),
+      fixtures: resolve(__dirname, "./fixtures"),
       tests: resolve(__dirname, "./tests"),
     },
   },
